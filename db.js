aa939ca7d8189e5d1b354f95640c40a2ca513d99
Jason Zhang <jasonzhang.syd@gmail.com>
2021-07-02 00:25:32 +1000

    adjust structure and add models

---
+++ db.js
@@ -0,0 +1,12 @@
+// 这里直接模拟db数据库，数据库server是个对象，里面有users和articles两张表（数组），users表里有一个个的user docu（对象）
+const db = {
+  users: [],
+};
+
+export default {
+  getData: () => db,
+  setData: (key, value) => {
+    db[key] = value;
+    return db;
+  },
+};
--- resolvers/userResolver.js
+++ resolvers/userResolver.js
@@ -1,11 +1,19 @@
 import { nanoid } from 'nanoid';
+import UserModel from '../Models/UserModel';
 
 export default {
   Query: {
-    user: () => ({
-      id: nanoid(),
-      name: 'joey',
-      age: 8,
-    }),
+    userList: () => {
+      UserModel.getUserList();
+    },
+  },
+  Mutation: {
+    createUser: (parent, args) => {
+      const user = new UserModel({
+        name: args.name,
+        age: args.age,
+      }).createUser();
+      return user;
+    },
   },
 };
